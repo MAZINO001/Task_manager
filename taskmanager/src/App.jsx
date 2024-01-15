4ae4c2c835ffc7a18f9043a8d6c605f904e8df5e
mounir <marnissimounir05@gmail.com>
2024-01-15 13:56:09 +0100

    adding dark mode

--- taskmanager/src/App.jsx
+++ taskmanager/src/App.jsx
@@ -1,16 +1,7 @@
-import "./app.css";
-import Header from "./components/Header";
-import SideBar from "./components/SideBar";
-import Wrapper from "./components/Wrapper";
-import Container from "./components/Container";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Wrapper>
-        <SideBar />
-        <Container />
-      </Wrapper>
+      
     </div>
   );
 }
