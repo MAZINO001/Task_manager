b118f8830b8a3f05a38ecd6e0838fb5944e6d5dc
mounir <marnissimounir05@gmail.com>
2024-01-15 19:33:31 +0100

    update project 5

--- taskmanager/src/Modals/AddEditBoardModal.jsx
+++ taskmanager/src/Modals/AddEditBoardModal.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { useState } from "react";
 export default function AddEditBoardModal({ setboardModalOpen, type }) {
-    useState
+  const [name, setName] = useState("");
+  const [newColumn, setNewColumn] = useState(
+    [
+        {name : 'Todo' , task : [] , id : ''},
+        {name : 'Todo' , task : [] , id : ''}
+    ]
+  );
   return (
     <div
       className="fixed right-0 left-0 bottom-0 top-0 px-2 scrollbar-hide py-4 overflow-scroll z-50 justify-center items-center flex bg-[#00000080]"
@@ -27,15 +33,21 @@ export default function AddEditBoardModal({ setboardModalOpen, type }) {
             Board Columns
           </label>
           <input
-            type="text"
-            className="bg-transparent px-4 py-2 rounded-md text-sm border border-gray-600 focus:outline-[#635fc7] outline-1 ring-0"
+            className="bg-transparent px-4 py-2 rounded-md text-sm border border-gray-600 outline-none focus:outline-[#635fc7] outline-1 ring-0"
             placeholder=" e.g Web Desing"
             value={name}
-            onChange={(e)=>{
-                setName(e.target.value)
+            onChange={(e) => {
+              setName(e.target.value);
             }}
-            id='border-name-input'
+            id="border-name-input"
           />
+        </div>
+        {/* board columns */}
+        <div className="mt-8 flex flex-col space-y-3 ">
+          <label className="text-sm dar:text-white text-gray-500">
+            Board Columns
+          </label>
+          {}
         </div>
       </div>
     </div>
