3d64313b3060a1ab67da7a744ad8ca4b0f53050c
mounir <marnissimounir05@gmail.com>
2024-01-17 18:32:42 +0100

    fixing update 8.3

--- taskmanager/src/components/Column.jsx
+++ taskmanager/src/components/Column.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { shuffle } from "lodash";
+import Task from "./Task";
 
 export default function Column({ colIndex }) {
   const colors = [
--- taskmanager/src/components/Task.jsx
+++ taskmanager/src/components/Task.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-export default function Task() {
-  return <div>Task</div>;
+export default function Task({ taskIndex, colIndex }) {
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive);
+  const columns = board.columns;
+  const col = board.columns.find((col, i) => i === colIndex);
+  const task = board.tasks.find((task, i) => i === taskIndex);
+
+  const [isTaskModalOpen, setisTaskModalOpen] = useState(false);
+  let completed = 0;
+  let subtasks = task.subtasks;
+  subtasks.forEach((subtask) => {
+    if (subtask.isCompleted) {
+      completed++;
+    }
+  });
+  return (
+    <div className="w-[280px] first:my-5 rounded-lg bg-white dark:bg-[#2b2c37] shadow-[#364e7e1a] py-6 px-3 shadow-lg hover:text-[#635fc7] dark:text-white dark:hover:text-[#635fc7] cursor-pointer "></div>
+  );
 }
