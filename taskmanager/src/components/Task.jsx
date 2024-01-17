f4297dd862598f93b73b48f3f274d57a65dba9ed
mounir <marnissimounir05@gmail.com>
2024-01-17 20:33:56 +0100

    update 8.4

--- taskmanager/src/components/Task.jsx
+++ taskmanager/src/components/Task.jsx
@@ -6,7 +6,7 @@ export default function Task({ taskIndex, colIndex }) {
   const board = boards.find((board) => board.isActive);
   const columns = board.columns;
   const col = board.columns.find((col, i) => i === colIndex);
-  const task = boards.tasks.find((task, i) => i === taskIndex);
+  const task = col.tasks.find((task, i) => i === taskIndex);
 
   const [isTaskModalOpen, setIsTaskModalOpen] = useState(false); // Corrected the function name
   let completed = 0;
@@ -18,16 +18,19 @@ export default function Task({ taskIndex, colIndex }) {
   });
 
   return (
-    <div className="w-[280px] first:my-5 rounded-lg bg-white dark:bg-[#2b2c37] shadow-[#364e7e1a] py-6 px-3 shadow-lg hover:text-[#635fc7] dark:text-white dark:hover:text-[#635fc7] cursor-pointer ">
-      <p
-      className="font-bold tracking-wide">
-        {task.title}
-      </p>
-      <p
-      className="font-bold text-xs tracking-tighter mt-2 text-gray-500 ">
-        {completed} of{subtasks.length} completed tasks
-      </p>
-
+    // first:my-5
+    <div>
+      <div className="w-[280px] my-5 rounded-lg bg-white dark:bg-[#2b2c37] shadow-[#364e7e1a] py-6 px-3 shadow-lg hover:text-[#635fc7] dark:text-white dark:hover:text-[#635fc7] cursor-pointer ">
+        <p className="font-bold tracking-wide">{task.title}</p>
+        <p className="font-bold text-xs tracking-tighter mt-2 text-gray-500 ">
+          {completed} of {subtasks.length} completed tasks
+        </p>
+      </div>
+      {
+        isTaskModalOpen && (
+            <TaskModal/>
+        )
+      }
     </div>
   );
 }
