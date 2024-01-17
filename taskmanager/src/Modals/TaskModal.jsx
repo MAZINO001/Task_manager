5b31662848954e5728d3f2477a52d5323770cc3c
mounir <marnissimounir05@gmail.com>
2024-01-17 20:53:28 +0100

    update 8.5

--- taskmanager/src/Modals/TaskModal.jsx
+++ taskmanager/src/Modals/TaskModal.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function TaskModal() {
+export default function TaskModal({ colIndex, taskIndex, setIsTaskModalOpen }) {
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive);
+  const columns = board.columns;
+  const col = board.columns.find((col, i) => i === colIndex);
+  const task = col.tasks.find((task, i) => i === taskIndex);
+  const subtasks = rask.subtasks;
+
+  let completed = 0;
+  subtasks.forEach((subtask) => {
+    if (subtask.isCompleted) {
+      completed++;
+    }
+  });
+  const [status, setstatus] = useState(second)
+
   return <div>TaskModal</div>;
 }
